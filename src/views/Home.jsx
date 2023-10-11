40164950bee578bdf2a9ff81bf700a1e8f9675df
Nathalia Acosta Vélez <nathalia285@gmail.com>
2023-10-11 16:10:31 -0500

    finalVersion

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,31 +1,27 @@
-import { useNavigate } from "react-router-dom";
-import "../sass/home.scss";
+import { useNavigate } from 'react-router-dom'
+import '../sass/home.scss'
 
 const App = () => {
-  const navigate = useNavigate();
+  const navigate = useNavigate()
 
   return (
-    <section className="home-container">
-      <div className="home-container__info">
-        <h3 className="home-container__info--subtitle">
-          SO, YOU WANT TO TRAVEL TO
-        </h3>
-        <h1 className="home-container__info--title">SPACE</h1>
-        <p className="home-container__info--text">
-          Let’s face it; if you want to go to space, you might as well genuinely
+    <section className='home-container'>
+      <div className='home-container__info'>
+        <h3 className='home-container__info--subtitle'>SO, YOU WANT TO TRAVEL TO</h3>
+        <h1 className='home-container__info--title'>SPACE</h1>
+        <p className='home-container__info--text'>Let’s face it; if you want to go to space, you might as well genuinely
           go to outer space and not hover kind of on the edge of it. Well sit
           back, and relax because we’ll give you a truly out of this world
-          experience!
-        </p>
+          experience!</p>
       </div>
-      <button
-        className="home-container__button"
-        onClick={() => navigate("/destination")}
+      <button 
+        className='home-container__button' 
+        onClick={() => navigate('/destination')}
       >
         EXPLORE
       </button>
     </section>
-  );
-};
+  )
+}
 
-export default App;
+export default App
